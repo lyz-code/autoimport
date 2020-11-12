620c97debc3bc43f0484ef3c4a4663fd8c6cccd2
Lyz <lyz@riseup.net>
2020-11-12 20:11:46 +0100

    fix(setup.py): extract the version from source without exec statement
    exec is known to generate vulnerabilities, use a regular expression
    instead.
    
    style(tests): use dedent to make the tests source code cleaner.

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,14 +1,19 @@
 """Tests the service layer."""
 
+from textwrap import dedent
+
 from autoimport.services import fix_code
 
 
 def test_fix_code_adds_missing_import() -> None:
     """Understands that os is a package and add it to the top of the file."""
     source = "os.getcwd()"
-    fixed_source = """import os
+    fixed_source = dedent(
+        """\
+        import os
 
-os.getcwd()"""
+        os.getcwd()"""
+    )
 
     result = fix_code(source)
 
@@ -26,18 +31,24 @@ def test_fix_doesnt_change_source_if_package_doesnt_exist() -> None:
 
 def test_fix_imports_packages_below_docstring() -> None:
     """Imports are located below the module docstrings."""
-    source = '''"""Module docstring
+    source = dedent(
+        '''\
+        """Module docstring
 
-"""
-import pytest
-os.getcwd()'''
-    fixed_source = '''"""Module docstring
+        """
+        import pytest
+        os.getcwd()'''
+    )
+    fixed_source = dedent(
+        '''\
+        """Module docstring
 
-"""
+        """
 
-import os
+        import os
 
-os.getcwd()'''
+        os.getcwd()'''
+    )
 
     result = fix_code(source)
 
@@ -46,15 +57,21 @@ os.getcwd()'''
 
 def test_fix_imports_packages_below_single_line_docstring() -> None:
     """Imports are located below the module docstrings when they only take one line."""
-    source = '''"""Module docstring."""
+    source = dedent(
+        '''\
+        """Module docstring."""
 
-import pytest
-os.getcwd()'''
-    fixed_source = '''"""Module docstring."""
+        import pytest
+        os.getcwd()'''
+    )
+    fixed_source = dedent(
+        '''\
+        """Module docstring."""
 
-import os
+        import os
 
-os.getcwd()'''
+        os.getcwd()'''
+    )
 
     result = fix_code(source)
 
@@ -63,12 +80,18 @@ os.getcwd()'''
 
 def test_fix_imports_type_hints() -> None:
     """Typing objects are initialized with their required header."""
-    source = """def function(dictionary: Dict) -> None:
-    pass"""
-    fixed_source = """from typing import Dict
+    source = dedent(
+        """\
+        def function(dictionary: Dict) -> None:
+            pass"""
+    )
+    fixed_source = dedent(
+        """\
+        from typing import Dict
 
-def function(dictionary: Dict) -> None:
-    pass"""
+        def function(dictionary: Dict) -> None:
+            pass"""
+    )
 
     result = fix_code(source)
 
@@ -79,9 +102,12 @@ def test_fix_substitutes_aliases() -> None:
     """Missing import statements are loaded from the alias if there is a match."""
     source = """getcwd()"""
     aliases = {"getcwd": "from os import getcwd"}
-    fixed_source = """from os import getcwd
+    fixed_source = dedent(
+        """\
+        from os import getcwd
 
-getcwd()"""
+        getcwd()"""
+    )
 
     result = fix_code(source, aliases)
 
@@ -100,8 +126,11 @@ def test_fix_doesnt_fail_if_object_not_in_aliases() -> None:
 
 def test_fix_removes_unneeded_imports() -> None:
     """If there is an import statement of an unused package it should be removed."""
-    source = """import requests
-foo = 1"""
+    source = dedent(
+        """\
+        import requests
+        foo = 1"""
+    )
     fixed_source = "foo = 1"
 
     result = fix_code(source)
@@ -111,8 +140,11 @@ foo = 1"""
 
 def test_fix_removes_unneeded_imports_in_from_statements() -> None:
     """Remove `from package import` statement of an unused packages."""
-    source = """from os import path
-foo = 1"""
+    source = dedent(
+        """\
+        from os import path
+        foo = 1"""
+    )
     fixed_source = "foo = 1"
 
     result = fix_code(source)
@@ -124,10 +156,16 @@ def test_fix_removes_unneeded_imports_in_beginning_of_from_statements() -> None:
     """Remove unused `object_name` in `from package import object_name, other_object`
     statements.
     """
-    source = """from os import path, getcwd
-getcwd()"""
-    fixed_source = """from os import getcwd
-getcwd()"""
+    source = dedent(
+        """\
+        from os import path, getcwd
+        getcwd()"""
+    )
+    fixed_source = dedent(
+        """\
+        from os import getcwd
+        getcwd()"""
+    )
 
     result = fix_code(source)
 
@@ -138,12 +176,18 @@ def test_fix_removes_unneeded_imports_in_middle_of_from_statements() -> None:
     """Remove unused `object_name` in
     `from package import other_object, object_name, other_used_object` statements.
     """
-    source = """from os import getcwd, path, mkdir
-getcwd()
-mkdir()"""
-    fixed_source = """from os import getcwd, mkdir
-getcwd()
-mkdir()"""
+    source = dedent(
+        """\
+        from os import getcwd, path, mkdir
+        getcwd()
+        mkdir()"""
+    )
+    fixed_source = dedent(
+        """\
+        from os import getcwd, mkdir
+        getcwd()
+        mkdir()"""
+    )
 
     result = fix_code(source)
 
@@ -154,10 +198,16 @@ def test_fix_removes_unneeded_imports_in_end_of_from_statements() -> None:
     """Remove unused `object_name` in `from package import other_object, object_name`
     statements.
     """
-    source = """from os import getcwd, path
-getcwd()"""
-    fixed_source = """from os import getcwd
-getcwd()"""
+    source = dedent(
+        """\
+        from os import getcwd, path
+        getcwd()"""
+    )
+    fixed_source = dedent(
+        """\
+        from os import getcwd
+        getcwd()"""
+    )
 
     result = fix_code(source)
 
