dd69c0fe7b6f6a728144022044f13acc67e866ad
hoverhell <hoverhell@gmail.com>
2021-12-08 20:01:20 +0300

    feat: cli `--config-file` option

--- src/autoimport/entrypoints/cli.py
+++ src/autoimport/entrypoints/cli.py
@@ -1,7 +1,7 @@
 """Command line interface definition."""
 
 import logging
-from typing import Tuple
+from typing import Optional, Tuple
 
 import click
 from maison.config import ProjectConfig
@@ -13,10 +13,14 @@ log = logging.getLogger(__name__)
 
 @click.command()
 @click.version_option(version="", message=version.version_info())
+@click.option("--config-file", default=None)
 @click.argument("files", type=click.File("r+"), nargs=-1)
-def cli(files: Tuple[str]) -> None:
+def cli(files: Tuple[str], config_file: Optional[str] = None) -> None:
     """Corrects the source code of the specified files."""
-    config = ProjectConfig(project_name="autoimport").to_dict()
+    config = ProjectConfig(
+        project_name="autoimport",
+        source_files=[config_file] if config_file else None,
+    ).to_dict()
     try:
         fixed_code = services.fix_files(files, config)
     except FileNotFoundError as error:
