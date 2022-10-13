3669babfc9253c62ff2c65947967a57d98cabfbc
Erik Bjäreholt <erik@bjareho.lt>
2022-10-13 13:29:46 +0200

    fix: ignore incorrect no-value-for-parameter lint

--- src/autoimport/__main__.py
+++ src/autoimport/__main__.py
@@ -3,4 +3,6 @@
 from autoimport.entrypoints.cli import cli
 
 if __name__ == "__main__":
-    cli()
+    # this needs no-value-for-parameter exclusion because pylint
+    # isn't smart enough to realize that click decorator handles it (but mypy is)
+    cli()  # pylint: disable=no-value-for-parameter
