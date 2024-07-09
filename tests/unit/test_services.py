8236098750d226719052d668040a31d68abef81a
Will Bradley <williambbradley@gmail.com>
2024-07-09 17:15:06 -0600

    updates

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -431,7 +431,8 @@ def test_fix_skips_moves_to_the_top_when_disabled_is_true() -> None:
 
 def test_fix_skips_moves_to_the_top_when_disabled_is_false() -> None:
     """Moving import statements should still occur when disable_move_to_top
-    config is false."""
+    config is false.
+    """
     source = dedent(
         """\
         import requests
